1466d37c243ba8a1e2fe5a9d857dbb954919f416
Bosiipo <olabosipo07@gmail.com>
2020-01-02 17:02:58 +0100

    better

--- models/Item.js
+++ models/Item.js
@@ -2,6 +2,12 @@ const db = require('../config/database');
 const Sequelize = require('sequelize');
 
 const Item = db.define('item', {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true
+  },
   name: {
     type: Sequelize.STRING,
     allowNull: false
